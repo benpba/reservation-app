3698afa824030eb5f6fbada27e536d88f8efb2ab
Benjamin Anokye <anokyebenpb@gmail.com>
2026-08-20 22:20:16 +0000

    Add frontend pages, request logging, and gitignore pem files

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} ${req.method} ${req.path}`);
+  next();
+});
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../client')));
+
 app.get('/', (req, res) => res.send('API is running'));
 
 const tablesRoute = require('./routes/tables');
